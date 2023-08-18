2349185ffc4e6e031ea15160fbabebde981c9d79
Nikhil Sarker <100969232+NikhilSarker@users.noreply.github.com>
2023-08-18 09:46:07 -0400

    Parallelogram calculation added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Triangle calculation
 let triangle_btn = document.getElementById('triangle_btn');
 triangle_btn.addEventListener('click', function(){
   // Get triangle base value
@@ -14,8 +15,11 @@ triangle_btn.addEventListener('click', function(){
   // Set the result
   let triangle_result = document.getElementById('triangle_result');
   triangle_result.innerText = result;
+  addToCalculationEntry('Triangle', result);
   
 })
+
+// Rectangle calculation
 let rectangle_btn = document.getElementById('rectangle_btn');
 rectangle_btn.addEventListener('click', function(){
   // Get triangle base value
@@ -32,13 +36,64 @@ rectangle_btn.addEventListener('click', function(){
   // Set the result
   let rectangle_result = document.getElementById('rectangle_result');
   rectangle_result.innerText = result;
+  addToCalculationEntry('Rectangle', result);
   
 })
+
+
+// parallelogram calculation
+
 let parallelogram_btn = document.getElementById('parallelogram_btn');
 parallelogram_btn.addEventListener('click', function(){
+  let parallelogram_base_value = getInputValue('parallelogram_base');
+  let parallelogram_height_value = getInputValue('parallelogram_height');
+  let result = parallelogram_base_value * parallelogram_height_value;
+  
+  setValue('parallelogram_result', result);
+
+  // Add to calculation entry
+  addToCalculationEntry('Parallelogram', result);
+
 
 })
 
+// Reuseable get input value filed in number
 function getInputValue(inputId){
+  let input = document.getElementById(inputId);
+  let inputIdText = input.value;
+  let inputIdValue = parseFloat(inputIdText);
+  input.value = '';
+  return inputIdValue;
+
+}
+
+// Reuseable set to a span, p (input value)
+function setValue(elementId, result){
+  let element = document.getElementById(elementId);
+  element.innerText = result;
+
+} 
+/*
+  1. Get the element where you want to add dynamic HTML.
+  2. Create an element you want to add.
+  3. If needed add some classes.
+  4. Set innerHTML it could be dynamic template string.
+  5. Append the child that you just created
+
+*/
+// Add to calculation entry
+function addToCalculationEntry(shapeType, result){
   
+  let calculation_entry = document.getElementById('calculation_entry');
+  let count = calculation_entry.childElementCount;
+  let paragraph = document.createElement('p');
+  paragraph.classList.add('my-5');
+  paragraph.classList.add('mx-2');
+  // paragraph.innerHTML = shapeType + ' '+ result;
+  paragraph.innerHTML = 
+  `
+  ${count + 1}. ${shapeType} ${result} cm<sup>2</sup> <button class="btn btn-success btn-sm ">Convert</button>
+  `;
+  calculation_entry.appendChild(paragraph);
+
 }
